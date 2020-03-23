6702ea034127bfac9902c3b2bef293538224aa9f
NoussDev <yannisleiseing@protonmail.com>
2020-03-23 07:27:10 +0100

    add react in the projet. 'Composer require encore', webpack encore settings with symfony docs, use yarn encore dev to compile the new js and css.

---
+++ assets/js/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class Title extends React.Component{
+    render(){
+        return(
+            <h1>This is my title </h1>
+        )
+    }
+}
+
+export default Title
+
--- public/React/Components/App.js
+++
@@ -1,9 +0,0 @@
-class App extends React.Component{
-    render(){
-        return(
-            <h1>React is here </h1>
-        )
-    }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
